056584cb1c31df7a9964d8b72b90a878fcb15a19
Ankit161409 <ankit161210@gmail.com>
2025-03-10 21:05:31 +0530

    Add files via upload

---
+++ arrays/multiple_of_2_matrixes.java
@@ -0,0 +1,87 @@
+
+import java.util.Scanner;
+
+class child
+{
+	public static void main(String[] args)
+	{
+	Scanner s=new Scanner(System.in);
+	 int i,j,k;
+	 System.out.println("enter the size of first matrix");
+	
+	 
+	 int m=s.nextInt();
+  
+	int n=s.nextInt();
+	 System.out.println("enter the size of second matrix");
+	 
+	 
+	int o=s.nextInt();
+ 
+	int p=s.nextInt();
+ 
+                  
+int x[][];
+int z[][];
+int y[][];
+
+x=new int[m][n];
+z=new int[o][p];
+y=new int[m][p];
+
+System.out.println("enter the value for first 3x3 matrix");
+for (   i=0; i<x.length; i++)
+{
+for (   j=0; j<x[i].length; j++)
+{
+	
+x[i][j]=s.nextInt();
+
+}	 
+}
+System.out.println();
+for (    i=0; i<m; i++)
+{
+for (   j=0; j<n; j++)
+{
+System.out.print(x[i][j]+" ");
+}
+System.out.println();
+}
+System.out.println();
+System.out.println("enter the value for 2nd 3x3 matrix");
+for (   i=0; i<o; i++)
+{
+for (   j=0; j<p; j++)
+{
+	
+z[i][j]=s.nextInt();
+
+}	 
+}
+System.out.println();
+
+for (   i=0; i<o; i++)
+{
+for (   j=0; j<p; j++)
+{
+System.out.print(z[i][j]+" ");
+}
+System.out.println();
+}
+System.out.println("the muktiple of these 2 matrices : ");
+System.out.println();
+for (   i=0; i<m; i++)
+{
+for (   j=0; j<p; j++)
+{
+	for (  k=0; k<o; k++)
+	y[i][j]+=x[i][k]*z[k][j];
+System.out.print(y[i][j]+" ");
+}
+System.out.println();
+}
+
+}}
+
+
