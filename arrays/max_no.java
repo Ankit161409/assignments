4162164a667c1aab8998f13a7460a5cb1d9d2993
Ankit161409 <ankit161210@gmail.com>
2025-03-10 18:07:42 +0530

    Add files via upload

---
+++ arrays/max_no.java
@@ -0,0 +1,28 @@
+ 
+ import java.util.Scanner;
+class child
+{
+
+public static void main(String[] args){
+ Scanner s = new Scanner(System.in);
+	
+		int z[];
+		System.out.println("enetr the size for arrays");	
+	int	 x= s.nextInt();  
+		z=new int[x];
+		System.out.println("enetr the values for arrays");	
+		 for (int i = 0; i < z.length; i++) {
+			 z[i]=s.nextInt();  
+		 }
+	int max=z[0];
+	 
+for ( int i=1; i<z.length; i++)
+{
+ 
+ if(z[i]>max)
+  max=z[i];
+  
+}
+    System.out.println("the larget number is : "+max);	
+}
+}
